bbc7834d02c4bdf1ebb241143baa07026abb0fd8
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2021-12-07 11:47:09 +0100

    Added views for comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
   validates :headline, presence: true
   validates :text, presence: true
+
+  # 1..1
+  # has_one :comment
+
+  # 1..N
+  has_many :comments
+
+  # N..M
+  # has_and_belongs_to_many :comments
 end
