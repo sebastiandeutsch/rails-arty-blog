af7ea79000248dd2be3a107de38cbe9491333f26
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2021-12-07 11:32:19 +0100

    Added migration

---
+++ db/migrate/20211207101824_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :comments do |t|
+      t.text :text
+      t.string :name
+      t.string :email
+
+      t.references :post
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_113939) do
+ActiveRecord::Schema.define(version: 2021_12_07_101824) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.string "name"
+    t.string "email"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "headline"
